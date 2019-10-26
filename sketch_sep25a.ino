6b39be89b9939997976b89844e05ef2610cfe97d
Mathias Tausen <mta@gomspace.com>
2019-10-26 02:54:18 +0200

    Initial "learning" mode (just print last 150 samples)

--- sketch_sep25a.ino
+++ sketch_sep25a.ino
@@ -5,32 +5,42 @@
 // First detection when difference between two downsampled samples is < DET_HL_THRESHOLD
 // (pos->neg)
 #define DET_HL_THRESHOLD -100
-// Next detection when voltage has slowly risen by DET_RISING_THRESHOLD, then fallen by
-// DET_FALLING_THRESHOLD
-#define DET_RISING_THRESHOLD 20
-#define DET_FALLING_THRESHOLD -20
 // Timeout in samples while waiting for rise/fall
 #define DET_TIMEOUT 100
-// Number of samples in delay line and number of samples we look back when detecting slow
-// rise/fall
-#define NHIST 15
+// Default number of samples in delay line and number of samples we look back when
+// detecting slow rise/fall.  Warning: Will read this many samples after the initial
+// downward spike before starting to scan for rising slope, so must not be too high. First
+// "real" rising slope is about 40 samples after the spike.
+#define DEFAULT_NLOOKBACK 15
+// Maximum history
+#define NHIST 150
 
-volatile bool adc_ready = false; // new sample ready
-volatile int16_t adc_value;      // the new sample
+// Next detection when voltage has slowly risen by det_rising_threshold, then fallen by
+// det_falling_threshold
+static int16_t det_rising_threshold = 20;
+static int16_t det_falling_threshold = -20;
 
 // Sample delay line
-int16_t adc_value_hist[NHIST];
-int8_t ptr = 0;
+static int16_t adc_value_hist[NHIST];
+static int16_t ptr;
+static int16_t lookback_rising = DEFAULT_NLOOKBACK,
+    lookback_falling = DEFAULT_NLOOKBACK;
+// Get largest of the two lookbacks
+#define LARGEST_LOOKBACK (lookback_rising > lookback_falling ? lookback_rising : lookback_falling)
+
+static volatile bool adc_ready = false; // new sample ready
+static volatile int16_t adc_value;      // the new sample
 
 // misc state
-uint16_t ndet = 0; // total number of detections
-bool first = true; // handle initialization
-uint16_t n_samples_in_state; // number of samples processed while in current state
+static bool first = true; // handle initialization
+static uint16_t n_samples_in_state; // number of samples processed while in current state
+static bool learning_mode = false; // learning voltage curve
 enum state_t {STATE_IDLE, // waiting for first sudden voltage drop
               STATE_FILL_DELAYLINE, // waiting for delayline to fill
               STATE_DETECTING_RISING, // waiting for slow rise in voltage
               STATE_DETECTING_FALLING}; // waiting for slow drop in voltage
-state_t state, state_prev; // current and previous state of state machine
+static state_t state, state_prev; // current and previous state of state machine
+static uint16_t ndet = 0; // total number of detections
 
 // ADC interrupt service routine
 ISR(ADC_vect)
@@ -100,8 +110,8 @@ void detection() {
     ndet++;
 }
 
-// Handle delay line pointer wraparound, assuming idx is never more than NHIST off
-static inline int8_t idx_wraparound(int8_t idx) {
+// Handle delay line pointer wraparound, assuming idx is never more than nlookback off
+static inline int16_t idx_wraparound(int16_t idx) {
     if (idx < 0) {
         return idx + NHIST;
     } else if (idx >= NHIST) {
@@ -113,12 +123,19 @@ static inline int8_t idx_wraparound(int8_t idx) {
 
 void loop() {
     if (Serial.available() > 0) {
-        Serial.read();
+        char in = Serial.read();
         Serial.print(ndet);
         Serial.print(",");
         Serial.print(state);
         Serial.print(",");
         Serial.println(n_samples_in_state);
+
+        if (in == 'l') {
+            // Start learning mode, starting by flushing the delay line
+            learning_mode = true;
+            first = true;
+            ptr = 0;
+        }
     }
 
     // Poll until sample ready and manage delayline ptr
@@ -153,37 +170,60 @@ void loop() {
 
     } else if (state == STATE_FILL_DELAYLINE) {
         // Wait for delay line to fill...
-        if (n_samples_in_state >= NHIST) {
-            state = STATE_DETECTING_RISING;
+        if ((int16_t)n_samples_in_state >= (learning_mode ? NHIST : LARGEST_LOOKBACK)) {
+            if (!learning_mode) {
+                // start looking for a rising slope now
+                state = STATE_DETECTING_RISING;
+            } else {
+                // in learning mode, just print all the samples instead
+                Serial.print("[");
+                for (unsigned int i = 0; i < NHIST; i++) {
+                    Serial.print(adc_value_hist[idx_wraparound(ptr+i)]);
+                    if (i != NHIST-1)
+                        Serial.print(",");
+                }
+                Serial.println("]");
+                // then reset and return to idle
+                learning_mode = false;
+                ptr = 0;
+                first = true;
+                state = STATE_IDLE;
+            }
         }
 
-    } else if (state == STATE_DETECTING_RISING || state == STATE_DETECTING_FALLING) {
+    } else if (state == STATE_DETECTING_RISING) {
         // Newest sample
         int16_t now = adc_value_hist[idx_wraparound(ptr-1)];
-        // The sample at index ptr is from NHIST samples ago
-        int16_t then = adc_value_hist[ptr];
 
-        if (state == STATE_DETECTING_RISING) {
-            // Waiting for significant rise in voltage between <now> and NHIST samples ago.
-            // This should either be the initial voltage rise where battery voltage settles
-            // to its nominal voltage (motor still running) or when the piston is released.
-            if ((now - then) > DET_RISING_THRESHOLD) {
-                state = STATE_DETECTING_FALLING;
-            } else if (n_samples_in_state > DET_TIMEOUT) {
-                // Timeout waiting for rising - go back to idle
-                state = STATE_IDLE;
-            }
+        // The sample from lookback_rising samples ago
+        int16_t then = adc_value_hist[idx_wraparound(ptr-lookback_rising)];
 
-        } else { // STATE_DETECTING_FALLING
-            // Waiting for significant drop in voltage between <now> and NHIST samples ago.
-            // This should be when the gears are drawing back the piston again.
-            if ((now - then) < DET_FALLING_THRESHOLD) {
-                detection(); // trigger detection
-                state = STATE_DETECTING_RISING;
-            } else if (n_samples_in_state > DET_TIMEOUT) {
-                // Timeout waiting for falling - go back to idle
-                state = STATE_IDLE;
-            }
+        // Waiting for significant rise in voltage between <now> and NHIST samples ago.
+        // This should either be the initial voltage rise where battery voltage settles
+        // to its nominal voltage (motor still running) or when the piston is released.
+        if ((now - then) > det_rising_threshold) {
+            state = STATE_DETECTING_FALLING;
+        } else if (n_samples_in_state > DET_TIMEOUT) {
+            // Timeout waiting for rising - go back to idle
+            state = STATE_IDLE;
         }
+
+    } else if (state == STATE_DETECTING_FALLING) {
+        // Newest sample
+        int16_t now = adc_value_hist[idx_wraparound(ptr-1)];
+
+        // The sample from lookback_falling samples ago
+        int16_t then = adc_value_hist[idx_wraparound(ptr-lookback_falling)];
+
+        // Waiting for significant drop in voltage between <now> and NHIST samples ago.
+        // This should be when the gears are drawing back the piston again.
+        if ((now - then) < det_falling_threshold) {
+            detection(); // trigger detection
+            state = STATE_DETECTING_RISING;
+        } else if (n_samples_in_state > DET_TIMEOUT) {
+            // Timeout waiting for falling - go back to idle
+            state = STATE_IDLE;
+        }
+
     }
 }
